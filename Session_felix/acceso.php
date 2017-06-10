bdb1f740baac8a9253d67fabb0df48d48fc0b1bc
Clair <acheron_1405@hotmail.com>
2017-06-10 18:40:59 -0500

    Borrado db inecesarias

--- Session_felix/acceso.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION["nombre"] = "Felix";
-$_SESSION["password"] = "delia";
-echo "Se han creado la variables de secion.";
-echo '<a href="./verificar.php"> Verificar varibles </a>';
-?>
